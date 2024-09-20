c42024f1ea4a220b12cf7bc2603bad9293b6374f
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-09-20 01:44:02 +0100

    Add Book Class

--- Branche_technique/Prototype/model/BookRepository.php
+++ Branche_technique/Prototype/model/BookRepository.php
@@ -9,13 +9,31 @@ class BookRepository {
     }
 
     public function getAllBooks() {
-        $json = file_get_contents($this->dataFile);
-        $booksData = json_decode($json, true);
-        $books = [];
-        foreach ($booksData as $bookData) {
-            $books[] = new Book($bookData['id'], $bookData['title'], $bookData['author']);
+        if (!file_exists($this->dataFile)) {
+            echo "Data file not found: " . $this->dataFile . "\n";
+            return []; // Return an empty array if the file doesn't exist
         }
-        return $books;
+
+        $jsonData = file_get_contents($this->dataFile); // Read the JSON file
+        $bookData = json_decode($jsonData); //Deserialize from JSON
+
+        // Create an array of Book objects
+        $books = [];
+        foreach ($bookData as $data) {
+            $books[] = new Book($data->id, $data->title, $data->author);
+        }
+
+        return $books; // Return the array of Book objects
+    }
+    public function saveBook($book) {
+        // Load existing books
+        $books = $this->getAllBooks();
+        $books[] = $book; // Add the new book
+    
+        // Save back to JSON file
+        $jsonData = json_encode($books); //Serialize to JSON
+        return file_put_contents($this->dataFile, $jsonData); // write the JSON file
     }
 }
+
 ?>
