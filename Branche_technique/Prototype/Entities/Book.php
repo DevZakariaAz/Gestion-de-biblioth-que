b2726d839a488bd66842c47ffefe24b650cfb96d
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-09-24 16:56:19 +0100

    Prototype

---
+++ Branche_technique/Prototype/Entities/Book.php
@@ -0,0 +1,42 @@
+<?php
+
+class Book
+{
+  private $id;
+  private $ISBN;
+  private $title;
+
+  public function __construct($ISBN, $title)
+  {
+    $this->id = time();
+    $this->ISBN = $ISBN;
+    $this->title = $title;
+  }
+
+
+
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function getISBN()
+  {
+    return $this->ISBN;
+  }
+
+  public function setISBN($ISBN)
+  {
+    $this->ISBN = $ISBN;
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  }
+}
