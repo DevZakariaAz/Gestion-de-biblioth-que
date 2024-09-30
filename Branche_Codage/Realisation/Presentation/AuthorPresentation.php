f4bfe917a09f4056209131f4947c7cea351c8e27
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2024-09-30 01:30:45 +0100

    Author Presentation

--- Branche_Codage/Realisation/Presentation/AuthorPresentation.php
+++ Branche_Codage/Realisation/Presentation/AuthorPresentation.php
@@ -25,6 +25,58 @@ class AuthorPresentation
     echo "---------------------------------\n\n";
   }
 
+  // public function editAuthor()
+  // {
+  //   echo "\nEditing an Author\n";
+  //   $authorId = askQuestion("Enter the ID of the author to edit (or type 'back' to go back): ");
+  //   if (strtolower($authorId) === "back") {
+  //       return;
+  //   }
+
+  //   $authorService = new AuthorService();
+  //   $authors = $authorService->getAuthors();
+
+  //   $foundAuthor = null; 
+  //   foreach ($authors as $author) {
+  //       if ($author->getId() == $authorId) {
+  //           $foundAuthor = $author;
+  //           break;
+  //       }
+  //   }
+
+  //   if (!$foundAuthor) {
+  //       echo "No author found with ID: $authorId\n\n";
+  //       return;
+  //   }
+
+  //   echo "Current details of the author:\n";
+  //   echo "---------------------------------\n";
+  //   echo "Id: " . $foundAuthor->getId() . "\n";
+  //   echo "Last Name: " . $foundAuthor->getlastName() . "\n";
+  //   echo "First Name: " . $foundAuthor->getfirstName() . "\n";
+  //   echo "Nationality: " . $foundAuthor->getnationality() . "\n";
+  //   echo "---------------------------------\n";
+
+  //   // Prompt the user for new details
+  //   $newLastName = askQuestion("Enter the new last name of the author (or press Enter to keep current last name): ");
+  //   if (!empty($newLastName)) {
+  //       $foundAuthor->setlastName($newLastName);
+  //   }
+
+  //   $newFirstName = askQuestion("Enter the new first name of the author (or press Enter to keep current first name): ");
+  //   if (!empty($newFirstName)) {
+  //       $foundAuthor->setfirstName($newFirstName);
+  //   }
+
+  //   $newNationality = askQuestion("Enter the new nationality of the author (or press Enter to keep current nationality): ");
+  //   if (!empty($newNationality)) {
+  //       $foundAuthor->setnationality($newNationality);
+  //   }
+
+  //   $authorService->setAuthor($foundAuthor);
+  //   echo "Author updated successfully\n\n";
+  // }
+  
   public function addAuthor()
   {
     echo "\nAdding a new Author\n";
